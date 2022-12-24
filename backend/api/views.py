58ddf6f6093be689e3935895c71809978815c3cf
Diego0614 <diego.munoz.arce@correounivalle.edu.co>
2022-12-24 00:09:15 -0500

    Mejora cálculo SS. (actualizar proyecto React)

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,21 +3,27 @@ import spacy
 
 # Create your views here.
 def api_home(request):
-    cadena = request.GET.get('user_stories','')
-    suma = 0
-    divisor = 0    
-    cadenas = cadena.split(sep='/')
-
     nlp = spacy.load("en_core_web_md")
-
-    for i in range(0, len(cadenas) - 1):
-        for j in range(i+1, len(cadenas)):
-            token1 = nlp(cadenas[0])
-            token2 = nlp(cadenas[1])
-            suma += token1.similarity(token2)
-            divisor += 1
+    arraySS = []
+    arrayCadena = request.GET.get('user_stories','')
+    arrayCadenas = arrayCadena.split(sep='*')
+    for h in range(0, len(arrayCadenas)):
+        suma = 0
+        divisor = 0
+        cadena = arrayCadenas[h]
+        cadenas = cadena.split(sep='/')
+        if len(cadenas) > 1:
+            for i in range(0, len(cadenas) - 1):
+                for j in range(i+1, len(cadenas)):
+                    token1 = nlp(cadenas[0])
+                    token2 = nlp(cadenas[1])
+                    suma += token1.similarity(token2)
+                    divisor += 1
+                    
+            semantic_similarity = suma / divisor
+        else:
+            semantic_similarity = 1
             
-    semantic_similarity = suma / divisor
-    data = {"semantic_similarity":semantic_similarity}
-    
+        arraySS.append(semantic_similarity)
+    data = {"semantic_similarity":arraySS}
     return JsonResponse(data)
