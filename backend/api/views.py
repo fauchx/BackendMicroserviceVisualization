fe7b9d45be217f573a68d4f0526f6937d88e6e24
Diego0614 <diego.munoz.arce@correounivalle.edu.co>
2023-02-24 00:08:52 -0500

    Se modificó el cálculo de la similitud semántica

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,17 @@
 from django.http import JsonResponse
 import spacy
 
-# Create your views here.
-def api_home(request):
+ingles = True
+
+if ingles:
     nlp = spacy.load("en_core_web_md")
+else:
+    nlp = spacy.load("es_core_news_md")
+
+
+def api_home(request):
     arraySS = []
-    arrayCadena = request.GET.get('user_stories','')
+    arrayCadena = request.GET.get('user_stories', '')
     arrayCadenas = arrayCadena.split(sep='*')
     for h in range(0, len(arrayCadenas)):
         suma = 0
@@ -15,15 +21,39 @@ def api_home(request):
         if len(cadenas) > 1:
             for i in range(0, len(cadenas) - 1):
                 for j in range(i+1, len(cadenas)):
-                    token1 = nlp(cadenas[0])
-                    token2 = nlp(cadenas[1])
+                    hu1 = seleccionarSustantivosVerbos(cadenas[0])
+                    token1 = nlp(hu1)
+                    hu2 = seleccionarSustantivosVerbos(cadenas[1])
+                    token2 = nlp(hu2)
                     suma += token1.similarity(token2)
                     divisor += 1
-                    
+
             semantic_similarity = suma / divisor
         else:
             semantic_similarity = 1
-            
+
         arraySS.append(semantic_similarity)
-    data = {"semantic_similarity":arraySS}
-    return JsonResponse(data) 
+    data = {"semantic_similarity": arraySS}
+    return JsonResponse(data)
+
+
+if ingles:
+    def seleccionarSustantivosVerbos(historia):
+        doc = nlp(historia)
+        listaText = ''
+        for token in doc:
+            pos = token.pos_
+            if pos == 'NOUN' or pos == 'PROPN':
+                listaText += token.text + ' '
+        return listaText
+else: 
+    def seleccionarSustantivosVerbos(historia):
+        doc = nlp(historia)
+        listaText = ''
+        for token in doc:
+            pos = token.pos_
+            if pos == 'NOUN':
+                listaText += token.text + ' '
+            else:
+                listaText += token.lemma_ + ' '
+        return listaText
